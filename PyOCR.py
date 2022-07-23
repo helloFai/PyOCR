f37dd0bb19f2ff989a26be7657b82fd8f778067a
helloFai <104523926+helloFai@users.noreply.github.com>
2022-07-23 21:00:51 +0200

    Update PyOCR.py

--- PyOCR.py
+++ PyOCR.py
@@ -1,27 +1,16 @@
 from pydoc import ErrorDuringImport
-from PIL import Image, ImageGrab
+from PIL import ImageGrab
 import pytesseract
 import pyperclip
 
-im = ImageGrab.grabclipboard()
 langslib = ["chi_sim", "chi_tra", "eng", "ita", "jpn"]
-
-# if isinstance(im, Image.Image):
-#     print("Image: size : %s, mode : %s" % (im.size, im.mode))
-#     im.save("./grab_clipboard.png")
-# elif im:
-#     for filename in im:    
-#         print("filename : %s" % filename)
-#         im = Image.open(filename)
-# else:
-#     print("clipboard is empty")
-
     
 def ocr_core(img, langu):
     text = pytesseract.image_to_string(img, lang=langu)
     return text
 
 def main():
+    im = ImageGrab.grabclipboard()
     try:
         picknum = int(input("please select a language:1. 简中 2.繁中 3.English 4.italiano 5.日本語:  "))
         lang_picked = langslib[picknum-1]
